4b7fc643425c04d530266f65e757e6aab85ab185
Dewys Eguis <deguis@mangus.org>
2021-10-04 17:23:27 -0500

    Reverse-String Terminado

--- reverse-string/reverse_string.py
+++ reverse-string/reverse_string.py
@@ -1,2 +1,10 @@
-def reverse(text):
-    return text[::-1]
+def distance(strand_a, strand_b):
+    if len(strand_a)!=len(strand_b):
+        raise ValueError("strand a must be same length as strand b")
+    cont = 0
+
+    for i in range(len(strand_a)):
+        if strand_a[i] != strand_b[i]:
+            cont += 1
+    return cont
+
